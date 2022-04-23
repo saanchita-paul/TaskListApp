54a1d375f62479533973394f69a86fe5da2ce46a
Sanchita Paul <68769067+saanchita-paul@users.noreply.github.com>
2022-04-23 20:32:14 +0600

    validation, readme file

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -27,25 +27,47 @@ class TaskController extends Controller
     public function store(Request $request)
     {
         // $user_id = Auth::user()->id;
+    //     if (Auth::user()->role == 1 || Auth::user()->role == 2) {
+    //         $data = new Task();
+    //         $data->name = $request->name;
+    //         $data->slug = $request->slug;
+    //         $data->description = $request->description;
+    //         $data->user_id = Auth::user()->id;
+    //         $data->save();
+    //     }
+    //    return response()->json([
+    //     'success' => true,
+    //     'message'=> "Data inserted succesfully",
+    //     'data' => $data,
+    //     ]);
         if (Auth::user()->role == 1 || Auth::user()->role == 2) {
-            // $request->validate([
-            //     'name'=>'required',
-            //     'slug'=>'required',
-            //     'description'=>'required'
-            // ]);
-            // $data = Task::create($request->all());
-            $data = new Task();
+            $validator = Validator::make($request->all(), [
+                'name' => 'required|string|max:100',
+                'description' => 'nullable|string',
+            ]);
+            if($validator->fails()) {
+            //$task['status'] = 'error';
+            if(empty($request->name)){
+                $task['name'] = 'error';
+            }
+            if(empty($request->description)){
+                $task['description'] = 'error';
+            }
+            $task['message']='Something is missing';
+            return json_encode($task);
+            }
+            $data = new Task;
             $data->name = $request->name;
-            $data->slug = $request->slug;
+            $data->slug = Str::slug($request->name, '-');
             $data->description = $request->description;
             $data->user_id = Auth::user()->id;
             $data->save();
         }
-       return response()->json([
-        'success' => true,
-        'message'=> "Data inserted succesfully",
-        'data' => $data,
-        ]);
+            return response()->json([
+                'success' => true,
+                'message'=> "Data inserted succesfully",
+                'data' => $data,
+            ]);
 
     }
 
@@ -53,13 +75,18 @@ class TaskController extends Controller
     {
         //$user_id = Auth::user()->id;
         if (Auth::user()->role == 1 || Auth::user()->role == 2) {
-        $task=Task::find($id);
-        $task->update($request->all());
+            $data = Task::find($id);
+            
+            $data->name = $request->name;
+            $data->slug = Str::slug($request->name, '-');
+            $data->description = $request->description;
+            //$data->user_id = Auth::user()->id;
+            $data->update($request->all());
         }
         return response()->json([
             'success' => true,
             'message'=> "Data updated succesfully",
-            'data' => $task,
+            'data' => $data,
             ]);
 
     }
@@ -85,7 +112,7 @@ class TaskController extends Controller
         }
         elseif(Auth::user()->role == 1){
             return response()->json([
-                'message'=> "You don't have permision to destroy this record."
+                'message'=> "You don't have permision to delete this record."
             ]);
         }
         return response()->json([
